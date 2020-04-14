d33f6a88139e1a4a6d3f199f175ca44ea6f9e404
Franklyn Ogbonna <franklynogbonna@Franklyns-MacBook-Pro.local>
2020-04-14 17:42:12 +0100

    final touch

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_restful import Api
 import src.estimator as estimator
-import time
+from datetime import timezone, datetime
 from simplexml import dumps
 
 # define the application
@@ -15,7 +15,7 @@ log_file = "logs.txt"
 def post():
     # get the json string from the request body
     req_data = request.get_json()
-    req_time = int(round(time.time() * 1000))
+    req_time = datetime.now(tz=timezone.utc).timestamp() * 1000
     route = request.path
     rsp = process(req_time, route, req_data)
     return jsonify(rsp)
@@ -25,11 +25,11 @@ def post():
 def post_xml():
     # get the json string from the request body
     req_data = request.get_json()
-    req_time = int(round(time.time() * 1000))
+    req_time =datetime.now(tz=timezone.utc).timestamp() * 1000
     route = request.path
     rsp = process(req_time, route, req_data)
     """Makes a Flask response with a XML encoded body"""
-    resp = make_response(dumps({'response': rsp}))
+    resp = make_response(dumps({'estimation': rsp}))
     resp.headers.extend({"mimetype": "text/xml"})
     return resp
 
@@ -38,7 +38,7 @@ def post_xml():
 def post_json():
     # get the json string from the request body
     req_data = request.get_json()
-    req_time = int(round(time.time() * 1000))
+    req_time = datetime.now(tz=timezone.utc).timestamp() * 1000
     route = request.path
     rsp = process(req_time, route, req_data)
     return jsonify(rsp)
@@ -49,7 +49,7 @@ def process(req_time, route, req_data):
         # pass the request data to the estimator function
         rsp = estimator.estimator(req_data)
         # return a json response to the browser
-        rsp_time = int(round(time.time() * 1000))
+        rsp_time = datetime.now(tz=timezone.utc).timestamp() * 1000
         diff_sec = (rsp_time - req_time) / 1000
         log = str(req_time) + "\t\t" + route + "\t\t done in " + str(diff_sec) + " seconds\n"
         append_log(log)
@@ -68,13 +68,12 @@ def logs():
         f = open(log_file, mode='rt', encoding='utf-8')
         for line in f.read():
             logging += line
+        f.close()
     except FileNotFoundError:
         error = {
             "message": "Log is currently empty"
         }
         return jsonify(error)
-    finally:
-        f.close()
     return logging
 
 
